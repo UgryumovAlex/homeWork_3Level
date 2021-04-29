217da1eed0d7c690c2f2072af07b25e3e5582f7c
a.ugryumov <ugryumov_alex@mail.ru>
2021-04-29 09:21:34 +0300

    Домашняя работа №5 Java3. Гонки машин

--- src/homeWork.java
+++ src/homeWork.java
@@ -1,5 +1,47 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Организуем гонки:
+ * Все участники должны стартовать одновременно, несмотря на то, что на подготовку у каждого из них уходит разное время.
+ * В туннель не может заехать одновременно больше половины участников (условность).
+ * Попробуйте всё это синхронизировать.
+ * Только после того как все завершат гонку, нужно выдать объявление об окончании.
+ * Можете корректировать классы (в т.ч. конструктор машин) и добавлять объекты классов из пакета util.concurrent.
+ * */
 public class homeWork {
+    public static final int CARS_COUNT = 4;
+
     public static void main(String[] args) {
-        //
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        CyclicBarrier step = new CyclicBarrier(CARS_COUNT + 1); //Все потоки машин плюс основной
+        ArrayBlockingQueue<Car> raceResults = new ArrayBlockingQueue<>(CARS_COUNT); //результат гонки
+
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), step, raceResults);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        try {
+            step.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"); //Стартовый свисток
+            step.await(); //Стартуем после свистка
+            step.await(); //ждём окончания гонки
+
+            System.out.println("Победитель гонки : " + raceResults.take().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
